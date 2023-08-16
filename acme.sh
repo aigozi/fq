d98b283ba0ff7f2d46f3824732a7349c955dd194
aigozi <74133335+aigozi@users.noreply.github.com>
2023-08-17 02:32:42 +0800

    Update acme.sh

--- acme.sh
+++ acme.sh
@@ -2,6 +2,7 @@
 #acme一键申请证书
 # Author: LaoMa
 read -p "请输入域名: " domain
-curl https://get.acme.sh | sh
-~/.acme.sh/acme.sh --register-account -m xsight00@gmail.com
-~/.acme.sh/acme.sh --issue -d $domain --standalone
+{ curl https://get.acme.sh | sh && \
+~/.acme.sh/acme.sh --register-account -m xsight00@gmail.com && \
+~/.acme.sh/acme.sh --issue -d $domain --standalone; } || \
+echo "执行过程中出现了错误，请检查输入的域名和网络连接。"
